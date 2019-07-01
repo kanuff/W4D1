1b226a5ca90d8b060ff49fb55370a76d5d228071
K. Gofron <makerman4@gmail.com>
2019-07-01 17:21:44 -0400

    Create artwork shares routes and controller

---
+++ First_Controller/app/controllers/artwork_shares_controller.rb
@@ -0,0 +1,25 @@
+class ArtworkSharesController < ApplicationController
+
+
+    def create
+        artwork_share = ArtworkShare.new(artwork_shares_params)
+        if artwork_share.save
+            render json: artwork_share
+        else
+            render json: artwork_share.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        artwork_share = ArtworkShare.find(params[:id])
+        render json: artwork_share
+        artwork_share.destroy
+    end
+
+
+
+    private
+    def artwork_shares_params
+        params.require(:artwork_shares).permit(:artwork_id, :viewer_id)
+    end
+end
--- First_Controller/config/routes.rb
+++ First_Controller/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:create, :destroy, :index, :show, :update]
-resources :artworks, only: [:create, :destroy, :index, :show, :update]
+  resources :artworks, only: [:create, :destroy, :index, :show, :update]
+  resources :artwork_shares, only: [:create, :destroy]
+
+
   # get 'users' => 'users#index', as: 'users'
   # post 'users' => 'users#create'
   # get 'users/new' => 'users#new', as: 'new_user'
